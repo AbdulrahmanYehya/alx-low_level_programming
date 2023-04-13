81e9674e03a03fc32f01c8b7ada151c08b9ea2d1
AbdulrahmanYehya <abdulrahmanyehya257@gmail.com>
2023-04-12 23:24:09 -0700

    ALX

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+void _print_rev_recursion(char *s);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_palindrome(char *s);
+int factorial(int n);
+#endif
