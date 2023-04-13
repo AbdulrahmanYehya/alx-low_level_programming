7efe359cd1d833b98f2e89f490aa7d6ff1ff9780
AbdulrahmanYehya <abdulrahmanyehya257@gmail.com>
2023-04-12 23:59:29 -0700

    Fixing 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,6 +11,12 @@ int is_prime_number(int n)
 		return (0);
 	return (actual_prime(n, n - 1));
 }
+/**
+ * actual_prime - checks if number is prime
+ * @n: number
+ * @i: iterator
+ * Return: 1 is true and 0 is false
+ */
 int actual_prime(int n, int i)
 {
 	if (i == 1)
