b80ede9193e5c1884830cf4e87649b11da023bf9
AbdulrahmanYehya <abdulrahmanyehya257@gmail.com>
2023-04-12 23:42:56 -0700

    Fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+ * _print_rev_recursion - prints a input but in recursion or backwards
+ * @s: the string that is going to be printed as output
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
